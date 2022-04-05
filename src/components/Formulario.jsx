630d3c0b2937b80fc8240eaa6f672a332591bcb3
Alberto <albertopimentel04@gmail.com>
2022-04-05 12:03:33 -0500

    Formulario completo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState } from "react";
 
 function Formulario() {
+  const [busqueda, setBusqueda] = useState({
+    artista: "",
+    cancion: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (Object.values(busqueda).includes("")) {
+      return alert("Todos los campos son obligatorios");
+    }
+  };
+
   return (
-    <div>Formulario</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <legend>Busca por Artistas y Cancion</legend>
+
+      <div className="form-grid">
+        <div>
+          <label htmlFor="">Artista</label>
+          <input
+            type="text"
+            name="artista"
+            placeholder="Nombre Artista"
+            value={busqueda.artista}
+            onChange={(e) =>
+              setBusqueda({ ...busqueda, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+
+        <div>
+          <label htmlFor="">Cancion</label>
+          <input
+            type="text"
+            name="cancion"
+            placeholder="Nombre Cancion"
+            value={busqueda.cancion}
+            onChange={(e) =>
+              setBusqueda({ ...busqueda, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <input type="submit" value="buscar" />
+      </div>
+    </form>
+  );
 }
 
-export default Formulario
+export default Formulario;
