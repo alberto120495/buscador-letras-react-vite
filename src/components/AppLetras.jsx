436aa382db679bfbbe74b89f5005f2cc0da94e2e
Alberto <albertopimentel04@gmail.com>
2022-04-05 11:54:08 -0500

    Inicio del proyecto

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Formulario from "./Formulario";
+
+function AppLetras() {
+  return (
+    <>
+      <header>Busqueda de letras de canciones</header>
+
+      <Formulario />
+      <main></main>
+    </>
+  );
+}
+
+export default AppLetras;
